efb3216d54f96dd48e234dbc86ce25d8204cbe83
Harrison Yelton <hayelton@iu.edu>
2018-06-26 10:15:34 -0400

    Update the heaing when the form is submitted

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const 
+const form = document.querySelector('form#flickForm')
+
+const changeHeading = function(ev) {
+    ev.preventDefault()
+    const heading = document.querySelector('h1')
+    heading.textContent = 'Click!'
+}
+
+form.addEventListener('submit', changeHeading)
